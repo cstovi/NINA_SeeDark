1b17bfc4b66817eeaf399004f11c4a70b712b2b3
Carl Stovell <carlstovell@gmail.com>
2026-05-19 18:19:06 +0100

    ui: rename Discord webhook labels and add detail tooltip

--- NINA.Plugin.SeeDark/Properties/AssemblyInfo.cs
+++ NINA.Plugin.SeeDark/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ An optional Discord webhook mirrors key events (skip decisions, capture results,
 Credit to @Ettaswell-Jon for single handedly exposing all the bugs in early versions!")]
 [assembly: AssemblyCompany("Carl Stovell")]
 [assembly: AssemblyProduct("NINA.Plugin.SeeDark")]
-[assembly: AssemblyVersion("1.19.0.0")]
-[assembly: AssemblyFileVersion("1.19.0.0")]
+[assembly: AssemblyVersion("1.19.1.0")]
+[assembly: AssemblyFileVersion("1.19.1.0")]
 [assembly: AssemblyMetadata("ChangelogURL", "https://github.com/cstovi/NINA_SeeDark/releases")]
 [assembly: Guid("A5E7F3C1-2D4B-4A8E-9F1C-3B6D7E8A0F2C")]
 [assembly: ThemeInfo(
